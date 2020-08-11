1e9df5a0e92f03b67a4f88b677eb1adf47448df2
vvsos1 <vvsos1@hotmail.co.kr>
2020-08-11 21:15:33 +0900

    Initial commit

---
+++ src/main/java/me/vvsos1/demobootaws/DemoBootAwsApplication.java
@@ -0,0 +1,13 @@
+package me.vvsos1.demobootaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBootAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoBootAwsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/vvsos1/demobootaws/DemoBootAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.vvsos1.demobootaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBootAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
